93219221540ffe0ca8006be5f2a6ad4105a762fc
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-02-03 15:01:21 +0300

    Update client

--- client.py
+++ client.py
@@ -1,26 +1,35 @@
 import os
+import time
+
 import requests
 
 from config import environment
-from server import Update
+from server import Update, FetchUpdatesResponse
 
 
-async def shutdown():
+def shutdown():
     os.system("shutdown /s /t 1")
 
 
-async def sleep():
+def sleep():
     os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
 
 
 while True:
-    response = requests.get(f"{environment.app_protocol}://{environment.app_host}:{environment.app_port}/updates/fetch")
-    data = response.json()
-    updates: list[Update] = data["updates"]
-    for i in updates:
+    try:
+        response = requests.get(f"{environment.app_protocol}://{environment.app_host}:{environment.app_port}/updates/fetch")
+    except requests.exceptions.RequestException as e:
+        print(e)
+        time.sleep(5)
+        continue
+
+    data = FetchUpdatesResponse(**response.json())
+    for i in data.updates:
         if i.command == "sleep":
             sleep()
         elif i.command == "shutdown":
             shutdown()
         else:
             pass
+
+    time.sleep(1)
