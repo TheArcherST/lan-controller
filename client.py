c5268d158694f3e23c373b73ca47c367613f0fc5
Mihail Shapovalov <mihailsapovalov05@gmail.com>
2024-02-03 14:36:40 +0300

    Fix client

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import os
 import requests
 
 from config import environment
+from server import Update
 
 
 async def shutdown():
@@ -12,8 +13,14 @@ async def sleep():
     os.system("rundll32.exe powrprof.dll,SetSuspendState 0,1,0")
 
 
-
 while True:
     response = requests.get(f"{environment.app_protocol}://{environment.app_host}:{environment.app_port}/updates/fetch")
     data = response.json()
-    updates = data["updates"]
+    updates: list[Update] = data["updates"]
+    for i in updates:
+        if i.command == "sleep":
+            sleep()
+        elif i.command == "shutdown":
+            shutdown()
+        else:
+            pass
